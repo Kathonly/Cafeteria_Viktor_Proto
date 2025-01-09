5c0ba12ef933afa2ef0fece155ea3055083d98ad
Kathonly <kathonly@hotmail.com>
2025-01-08 22:10:13 -0500

    animaciones carrusel listas

--- public/scripts/Script.js
+++ public/scripts/Script.js
@@ -82,20 +82,13 @@ function obtener_platos() {
 
 function mostrar_carrusel() {
   const carrusel = document.getElementById("menu_carusell");
-  carrusel.innerHTML = ""; //Limpiar el contenido anterior.
-
-  let numero_tarjetas = 3;
-  if (ancho_pantalla < 1250 && ancho_pantalla > 650) {
-    numero_tarjetas = 2;
-  }
-  if (ancho_pantalla < 650) {
-    numero_tarjetas = 1;
-  }
+  // carrusel.innerHTML = ""; //Limpiar el contenido anterior.
+  let numero_tarjetas = 5;
 
   for (let i = 0; i < numero_tarjetas; i++) {
     const index = (carrusel_index + i) % platos_disponibles.length;
     const plato = platos_disponibles[index];
-    const tarjeta = document.createElement("div");
+    const tarjeta = document.getElementById(`plato_menu_${i}`);
     // console.log(plato[index].Descripcion);
     //aqui se manejan los datos Binarios de las imagenes de los platos.
     const blob = new Blob([new Uint8Array(plato.ImageData.data)], {
@@ -103,14 +96,19 @@ function mostrar_carrusel() {
     });
     const url = URL.createObjectURL(blob); //este nuevo URL se usa como src para la imagen en la tarjeta.
     //fin del manejo de los datos binarios.
-    tarjeta.className = "plato_menu";
-    tarjeta.id = `plato_menu_${i}`;
-    tarjeta.innerHTML = `
-            <h3>${plato.Descripcion}</h3>
-            <img src=${url} alt="imagen del plato">
-            <p>Precio: $ ${plato.Precio_Unidad}</p>
+    // tarjeta.className = `plato_menu_${i}`;
+    // tarjeta.id = `plato_menu_${i}`;
+    if (tarjeta.innerHTML.trim() === "") {
+      // console.clear();
+      // console.log(tarjeta.id);
+
+      tarjeta.innerHTML = `
+            <h3 class="nombre">${plato.Descripcion}</h3>
+            <img class="imagen" src=${url} alt="imagen del plato">
+            <p class="precio">Precio: $ ${plato.Precio_Unidad}</p>
         `;
-    carrusel.appendChild(tarjeta);
+    }
+    // carrusel.appendChild(tarjeta);
   }
 }
 
@@ -120,16 +118,60 @@ function upd_size() {
   // console.log(ancho_pantalla);
 }
 function carrusel_siguiente() {
+  let carrusel = document.getElementById("menu_carusell");
+  let tarjeta_aux_der = document.getElementById("plato_menu_4");
+  let tarjeta_izq = document.getElementById("plato_menu_1");
+  let tarjeta_cen = document.getElementById("plato_menu_2");
+  let tarjeta_der = document.getElementById("plato_menu_3");
+
+  carrusel.removeChild(document.getElementById("plato_menu_0"));
+  tarjeta_izq.className = "tarjeta_aux_izq";
+  tarjeta_izq.id = "plato_menu_0";
+  tarjeta_cen.className = "tarjeta_izq";
+  tarjeta_cen.id = "plato_menu_1";
+  tarjeta_der.className = "tarjeta_cen";
+  tarjeta_der.id = "plato_menu_2";
+  tarjeta_aux_der.className = "tarjeta_der";
+  tarjeta_aux_der.id = "plato_menu_3";
+  const new_plato_4 = document.createElement("div");
+  new_plato_4.id = "plato_menu_4";
+  new_plato_4.className = "tarjeta_aux_der";
+  carrusel.appendChild(new_plato_4);
+
+  // algo
   carrusel_index = (carrusel_index + 1) % platos_disponibles.length;
   mostrar_carrusel();
 }
 
 function carrusel_anterior() {
-  console.log("anterior");
+  // console.log("anterior");
+  let carrusel = document.getElementById("menu_carusell");
+  let tarjeta_aux_izq = document.getElementById("plato_menu_0");
+  let tarjeta_izq = document.getElementById("plato_menu_1");
+  let tarjeta_cen = document.getElementById("plato_menu_2");
+  let tarjeta_der = document.getElementById("plato_menu_3");
+  // let tarjeta_aux_der = document.getElementById("plato_menu_4");
+
+  carrusel.removeChild(document.getElementById("plato_menu_4"));
+  tarjeta_der.className = "tarjeta_aux_der";
+  tarjeta_der.id = "plato_menu_4";
+  tarjeta_cen.className = "tarjeta_der";
+  tarjeta_cen.id = "plato_menu_3";
+  tarjeta_izq.className = "tarjeta_cen";
+  tarjeta_izq.id = "plato_menu_2";
+  tarjeta_aux_izq.className = "tarjeta_izq";
+  tarjeta_aux_izq.id = "plato_menu_1";
+  const new_plato_0 = document.createElement("div");
+  new_plato_0.id = "plato_menu_0";
+  new_plato_0.className = "tarjeta_aux_izq";
+  carrusel.insertBefore(new_plato_0, carrusel.firstChild);
+  // carrusel.appendChild(new_plato_0);
+
+  // tarjeta_aux_der.className = ''
   carrusel_index =
     (carrusel_index - 1 + platos_disponibles.length) %
     platos_disponibles.length;
-  // animar_izq();
+
   mostrar_carrusel();
 }
 
@@ -149,6 +191,6 @@ function animar_izq() {
 var platos_disponibles = [];
 let ancho_pantalla = window.innerWidth;
 let carrusel_index = 0;
-let win_resized = false;
+// let win_resized = false;
 window.addEventListener("resize", upd_size);
 window.addEventListener("load", iniciar);
